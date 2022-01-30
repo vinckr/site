1f3825ed272e9024281547d51289d8bb9d8c900e
vinckr <vincent@ory.sh>
2022-01-30 19:35:43 +0100

    init

---
+++ src/links.ts
@@ -0,0 +1,31 @@
+import fs from "fs-extra";
+import { markdown } from "./utils/markdown.js";
+
+(async function () {
+  console.log("Converting Markdown to HTML...");
+
+  // markdown source
+  const content = await fs.readFile("./content/links.md", "utf8");
+
+  // converted to HTML
+  const rendered = await markdown.render(content);
+
+
+  const htmlFile = `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+  <meta charset="UTF-8" />
+  <title>Link Collection - vinckr.com</title>
+  <link rel="stylesheet" href="./styles/default.css">
+  </head>
+  <body>
+  ${rendered}
+  </body>
+  </html>`;
+
+  await fs.mkdirs("./public");
+
+  await fs.writeFile("./public/links.html", htmlFile, "utf8");
+
+  console.log("HTML generated.");
+})();

a9f3717565e964592eebbd42c1ead260e6c902c4
vinckr <vincent@ory.sh>
2022-04-06 23:56:02 +0200

    fix: remove login from nav

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,10 +20,6 @@ module.exports = {
         {
             name:'reviews',
             link:'/reviews'
-        },
-        {
-          name:'login',
-          link:'/login'
         }
       ]
   },

92681927de93d01a9dd969d9dce1d09f8af52225
Vincent Kraus <mail@vinckr.com>
2023-01-14 15:18:24 +0100

    u

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ set -e
 
 echo "Install build tools"
 go get github.com/vinckr/gokesh/cmd/build
+go install github.com/vinckr/gokesh/cmd/build
 echo "Building HTML files"
 go run vinckr/gokesh/cmd/build/ page index
 go run vinckr/gokesh/cmd/build/ page about
